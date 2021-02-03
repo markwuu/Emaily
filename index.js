39e1167911e350a57647ed5aee53f2e354ce7301
Mark W <markwuu@gmail.com>
2021-02-02 21:12:40 -0800

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
-});
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`app listening on port ${PORT}`);
