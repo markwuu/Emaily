a49fd2163b211b3632610c44877ef4d18a693f85
Mark W <markwuu@gmail.com>
2021-02-02 22:02:28 -0800

    Added google auth route

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+const { googleClientID, googleClientSecret } = keys;
+
 const app = express();
 
+passport.use(
+    new GoogleStrategy({
+        clientID: googleClientID,
+        clientSecret: googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken) => {
+        console.log('accessToken', accessToken);
+    })
+);
+
+//User clicks login and request is sent to Google
+//'google' string passed as argument, so Passport knows to use Google Strategy listed above
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`app listening on port ${PORT}`);
-});
+app.listen(PORT, () => { console.log(`app listening on port ${PORT}`) });
