be57f371f13ba8e00d2dcca6f8f34c039a36723e
Mark W <markwuu@gmail.com>
2021-02-06 15:11:33 -0800

    Created User model and created record when visiting google auth route

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,10 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 const { googleClientID, googleClientSecret } = keys;
+
+const User = mongoose.model('users');
 
 passport.use(
     new GoogleStrategy({
@@ -9,9 +12,7 @@ passport.use(
         clientSecret: googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken: ', accessToken);
-        console.log('refreshToken: ', refreshToken);
-        console.log('profile: ', profile);
-        console.log('done: ', done);
+        //Create new model instance and then persist it to the db
+        new User({ googleId: profile.id }).save();
     })
 );
