da7e9406bc2e73c0a386119d6383417413a477f9
Mark W <markwuu@gmail.com>
2021-02-10 19:04:27 -0800

    Imported materialize css and created header component with google login

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <nav>
+            <div class="nav-wrapper">
+            <a href="/" class="left brand-logo">Emaily</a>
+            <ul class="right">
+                <li>
+                    <a href="/auth/google">Login With Google</a>
+                </li>
+            </ul>
+            </div>
+        </nav>
     )
 }
 
