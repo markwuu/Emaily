915924bff819c71c3bb9e5df0592e0a0a47632cd
Mark W <markwuu@gmail.com>
2021-02-13 11:25:29 -0800

    Added descriptions to stripe payment component

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -4,6 +4,8 @@ import StripeCheckout from 'react-stripe-checkout';
 const Payments = () => {
     return (
         <StripeCheckout
+            name="Emaily"
+            description="$5 for 5 email credits"
             amount={500}
             token={token => console.log(token)}
             stripeKey={process.env.REACT_APP_STRIPE_KEY}
