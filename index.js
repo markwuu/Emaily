e31d3aec34855caef04b1e3bf42baa5435ab908d
Mark W <markwuu@gmail.com>
2021-02-17 20:57:31 -0800

    Created survey model and imported it into app file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 const { cookieKey } = keys;
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true})
