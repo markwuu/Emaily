8323c597ea9cb1b807fbc607bd2d6fe9b632c19b
Mark W <markwuu@gmail.com>
2021-02-25 21:37:17 -0800

    Redirected user to survey route on button click

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
         <div>
             Dashboard
             <div className="fixed-actionbtn">
-                <a className="btn-floating btn-large waves-effect waves-light red">
+                <Link to="/surveys/new" className="btn-floating btn-large waves-effect waves-light red">
                     <i class="material-icons">add</i>
-                </a>
+                </Link>
             </div>
         </div>
     )
