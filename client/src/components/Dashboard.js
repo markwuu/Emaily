85dae91254db30d8622f738b49c71dded6bce123
Mark W <markwuu@gmail.com>
2021-02-25 21:32:07 -0800

    Added material icons

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,6 +4,11 @@ const Dashboard = () => {
     return (
         <div>
             Dashboard
+            <div className="fixed-actionbtn">
+                <a className="btn-floating btn-large waves-effect waves-light red">
+                    <i class="material-icons">add</i>
+                </a>
+            </div>
         </div>
     )
 };
