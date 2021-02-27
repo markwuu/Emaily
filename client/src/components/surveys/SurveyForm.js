196ca35f99341990881663f2a0887ba5fcbeb4d5
Mark W <markwuu@gmail.com>
2021-02-26 23:10:25 -0800

    Created form components

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,13 @@
+//SurveyForm shows a form for a user to add input
+import React from 'react';
+import { Form, Field } from 'react-final-form'
+
+const SurveyForm = () => {
+    return (
+        <div>
+            The Survey Form Component!
+        </div>
+    )
+}
+
+export default SurveyForm;
---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,13 @@
+// SurveyNew shows SurveyForm and SurveyFormReview
+import React from 'react';
+import SurveyForm from '../surveys/SurveyForm';
+
+const SurveyNew = () => {
+    return (
+        <div>
+            <SurveyForm/>
+        </div>
+    )
+}
+
+export default SurveyNew;
