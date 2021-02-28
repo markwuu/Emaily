9e6a8ff08a3cd26b602ceabaa64602489d9fb707
Mark W <markwuu@gmail.com>
2021-02-27 20:01:56 -0800

    Created survey form with react final form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,10 +2,46 @@
 import React from 'react';
 import { Form, Field } from 'react-final-form'
 
-const SurveyForm = () => {
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const onSubmit = async values => {
+    await sleep(300)
+    // window.alert(JSON.stringify(values, 0, 2))
+    console.log(JSON.stringify(values, 0, 2));
+}
+
+const SurveyForm = (props) => {
+    const { handleSubmit, pristine, reset, submitting } = props;
     return (
         <div>
-            The Survey Form Component!
+        <Form
+            onSubmit={onSubmit}
+            render={({ handleSubmit, form, submitting, pristine, values }) => (
+                <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Survey Title</label>
+                    <Field
+                        name="surveyTitle"
+                        component="input"
+                        type="text"
+                        placeholder="Survey Title"
+                    />
+                </div>
+                <div className="buttons">
+                    <button type="submit" disabled={submitting || pristine}>
+                        Submit
+                    </button>
+                    <button
+                        type="button"
+                        onClick={form.reset}
+                        disabled={submitting || pristine}
+                    >
+                        Reset
+                    </button>
+                </div>
+                </form>
+            )}
+            />
         </div>
     )
 }
